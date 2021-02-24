bb6640ec6c26ff4d7fdaf2d1bc9a4c6cbe42f322
Simon Maher <simonmaher8307@hotmail.com>
2021-02-24 20:11:32 +0200

    added IF sttement to buttons and progress bar

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
+/*Create variables for project */
+
 const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
-
 let currentActive = 1
+
+/*End of variables */
+
+/*Functions for next and prev buttons */
 
 next.addEventListener('click', () => {
     currentActive++
-
     if(currentActive > circles.length) {
         currentActive = circles.length
-
     }
-    console.log(currentActive);
+    update()
 })
 prev.addEventListener('click', () => {
     currentActive--
-
     if(currentActive < 1) {
         currentActive = 1
-
     }
-    console.log(currentActive);
+    update()
 })
+function update() {
+    
+    circles.forEach((circle, index) => {
+        if(index < currentActive) {
+            circle.classList.add('active')
+        } else {
+            circle.classList.remove('active')
+        }
+        
+    })
+    /*End of functions */
+    
+    /*Const and IF statment for buttons and progress bar */
+    const actives = document.querySelectorAll('.active')
+
+    progress.style.width = (actives.length -1) / (circles.length -1) * 100 + '%'
+
+    if(currentActive === 1){
+        prev.disabled = true
+    }else if (currentActive === circles.length) {
+        next.disabled = true
+    }else {
+        prev.disabled = false
+        next.disabled = false
+    }
+}
+        
+    
+    
+    
+
+
+        
+    
